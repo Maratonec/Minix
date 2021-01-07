7bac0c78f01926d1525d3909c1f5a5f3254c9f90
Maratonec <maratonec.official@gmail.com>
2021-01-07 16:25:00 +0100

    Loading different program into memory
    Boot loader can now load different program into memory

--- Minix/Source/bootloader.asm
+++ Minix/Source/bootloader.asm
@@ -1,15 +1,20 @@
 
 [org 0x7c00]
 
+mov [BOOT_DISK], dl
+
 mov bp, 0x7c00
 mov sp, bp
 
 mov bx, BootString
 call PrintString
 
-jmp $
+call ReadDisk
+
+jmp PROGRAM_SPACE
 
 %include "../Minix/Source/Util/StringPrinter.asm"
+%include "../Minix/Source/Util/DiskReader.asm"
 
 times 510-($-$$) db 0
 
--- compiling/compilerasm.bat
+++ compiling/compilerasm.bat
@@ -1,3 +1,7 @@
-nasm ../Minix/Source/bootloader.asm -f bin -o bootloader.flp
+nasm ../Minix/Source/bootloader.asm -f bin -o bootloader.bin
+
+nasm ../Minix/Source/Util/ExtendedProgram.asm -f bin -o ExtendedProgram.bin
+
+copy /b bootloader.bin+ExtendedProgram.bin  bootloader.flp
 
 pause
